fcb9dd65d483d025c3df313e8930cbe6b59c8656
Sihun <tkgns01234@gmail.com>
2024-05-13 18:46:11 +0900

    Header파일 작업

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
 export default function Header({ children }) {
+    const handleSearch = (query) => {
+        console.log('검색어:', query);
+    };
+
     return (
         <header>
-            Header
+            <div className="logo">로고</div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/auth">Auth</a>
+                        <ul>
+                            <li><a href="/auth/register">회원가입</a></li>
+                            <li><a href="/auth/login">로그인</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="/user">User</a>
+                        <ul>
+                            <li><a href="/user/my-account">내 계정</a></li>
+                            <li><a href="/user/shopping-cart">장바구니</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <input type="text" placeholder="Search..." onChange={(e) => handleSearch(e.target.value)} />
             {children}
         </header>
     );
