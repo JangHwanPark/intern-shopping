3ea4728560f912e4803338002f7848b9efa3370f
kkambbab <166172297+kkambbab@users.noreply.github.com>
2024-05-13 22:36:53 +0900

    counter (#12)
    * refactoring: SQL 데이터 수정 (#6)
    
    * init
    
    * counter
    
    * Delete .idea/dataSources.xml
    
    * Delete client/src/assets/scss/index.css.map
    
    ---------
    
    Co-authored-by: japark <67992955+JangHwanPark@users.noreply.github.com>
    Co-authored-by: japark <wkd5878@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {AuthProvider} from "./context/AuthContext";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import GridLayout from "./layout/GridLayout";
+import Counter from "./components/Counter/Counter";
 
 function App() {
     return (
@@ -11,7 +12,9 @@ function App() {
                 <Header/>
                 <Outlet/>
                 <Footer/>
+                <Counter/>
             </GridLayout>
+            <Counter/>
         </AuthProvider>
     );
 }
--- client/src/components/Counter/Counter.jsx
+++ client/src/components/Counter/Counter.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Counter() {
+    const [Like, setLike] = useState(0);
+    const [number, setNumber] = useState(0);
+
+    const onIncrease = () => {
+        setNumber(number+1);
+    }
+    const onDecrease = () => {
+        setNumber(number-1);
+    }
+
     return (
         <div>
-            
+            <div onClick={() => {
+                setLike(Like + 1)
+            }}> ❤️ {Like}</div>
+
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
         </div>
     );
 }
